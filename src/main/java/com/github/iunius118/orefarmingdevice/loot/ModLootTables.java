31cc2cb046e08244a562e3cebfdcaa7d974e4d4b
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2021-11-18 20:33:56 +0900

    Add method to ModLootTables

--- src/main/java/com/github/iunius118/orefarmingdevice/loot/ModLootTables.java
+++ src/main/java/com/github/iunius118/orefarmingdevice/loot/ModLootTables.java
@@ -1,26 +1,40 @@
 package com.github.iunius118.orefarmingdevice.loot;
 
 import com.github.iunius118.orefarmingdevice.OreFarmingDevice;
+import com.github.iunius118.orefarmingdevice.world.level.block.entity.OFDeviceType;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 
+import java.util.function.Function;
+
 public enum ModLootTables {
-    DEVICE_0("device_0"),
-    DEVICE_0_NETHER("device_0_n"),
+    DEVICE_0("device_0", Blocks.COBBLESTONE.asItem(), t -> t == OFDeviceType.MOD_0),
+    DEVICE_0_NETHER("device_0_n", Blocks.NETHERRACK.asItem(), t -> t == OFDeviceType.MOD_0),
 
-    DEVICE_1("device_1"),
-    DEVICE_1_NETHER("device_1_n"),
+    DEVICE_1("device_1", Blocks.COBBLESTONE.asItem(), t -> t == OFDeviceType.MOD_1),
+    DEVICE_1_NETHER("device_1_n", Blocks.NETHERRACK.asItem(), t -> t == OFDeviceType.MOD_1),
 
-    DEVICE_2("device_2"),
-    DEVICE_2_NETHER("device_2_n"),
+    DEVICE_2("device_2", Blocks.COBBLESTONE.asItem(), t -> t == OFDeviceType.MOD_2),
+    DEVICE_2_NETHER("device_2_n", Blocks.NETHERRACK.asItem(), t -> t == OFDeviceType.MOD_2),
     ;
 
     private final ResourceLocation id;
+    private final Item item;
+    private final Function<OFDeviceType, Boolean> canProcess;
 
-    ModLootTables(String key) {
+    ModLootTables(String key, Item item, Function<OFDeviceType, Boolean> canProcess) {
         id = new ResourceLocation(OreFarmingDevice.MOD_ID, key);
+        this.item = item;
+        this.canProcess = canProcess;
     }
 
     public ResourceLocation getID() {
         return id;
+    }
+
+    public boolean canProcess(OFDeviceType type, ItemStack stack) {
+        return canProcess.apply(type) && stack.getItem() == item ;
     }
 }
