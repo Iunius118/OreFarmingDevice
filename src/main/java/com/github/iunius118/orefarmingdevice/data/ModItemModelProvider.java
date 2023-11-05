f2d035843304faca420a6ff4f3af45f245369ad7
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2023-11-06 00:45:46 +0900

    Add item models of cobblestone feeders

--- src/main/java/com/github/iunius118/orefarmingdevice/data/ModItemModelProvider.java
+++ src/main/java/com/github/iunius118/orefarmingdevice/data/ModItemModelProvider.java
@@ -1,9 +1,11 @@
 package com.github.iunius118.orefarmingdevice.data;
 
 import com.github.iunius118.orefarmingdevice.OreFarmingDevice;
+import com.github.iunius118.orefarmingdevice.world.item.ModItems;
 import com.github.iunius118.orefarmingdevice.world.level.block.ModBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.item.Item;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
@@ -18,6 +20,14 @@ public class ModItemModelProvider  extends ItemModelProvider {
         registerBlockItemModel(ModBlocks.DEVICE_0);
         registerBlockItemModel(ModBlocks.DEVICE_1);
         registerBlockItemModel(ModBlocks.DEVICE_2);
+        registerItemModel(ModItems.COBBLESTONE_FEEDER);
+        registerItemModel(ModItems.COBBLESTONE_FEEDER_2);
+    }
+
+    private void registerItemModel(Item item) {
+        String path = item.getRegistryName().getPath();
+        getBuilder(path).parent(new ModelFile.UncheckedModelFile("item/generated"))
+                .texture("layer0", "item/" + path);
     }
 
     private void registerBlockItemModel(Block block) {
