b0f46405fa8a22e173e9b406118048837c1e9624
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2023-11-09 20:10:59 +0900

    Migrate to SLF4J

--- src/main/java/com/github/iunius118/orefarmingdevice/OreFarmingDevice.java
+++ src/main/java/com/github/iunius118/orefarmingdevice/OreFarmingDevice.java
@@ -5,6 +5,7 @@ import com.github.iunius118.orefarmingdevice.common.RegisterEventHandler;
 import com.github.iunius118.orefarmingdevice.common.ServerProxy;
 import com.github.iunius118.orefarmingdevice.config.OreFarmingDeviceConfig;
 import com.github.iunius118.orefarmingdevice.data.*;
+import com.mojang.logging.LogUtils;
 import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
@@ -13,14 +14,13 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
 
 @Mod(OreFarmingDevice.MOD_ID)
 public class OreFarmingDevice {
     public static final String MOD_ID = "orefarmingdevice";
     public static final String MOD_NAME = "O.F.Device";
-    public static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public static ServerProxy proxy = DistExecutor.safeRunForDist(() -> ClientProxy::new, () -> ServerProxy::new);
 
