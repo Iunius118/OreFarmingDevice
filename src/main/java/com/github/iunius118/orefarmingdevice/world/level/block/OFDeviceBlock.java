2cf8834b1b3efd5ae69452ef23946ba5e8220b20
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2023-11-11 03:12:13 +0900

    Add block property `casing` to OF Devices to change appearance of device

--- src/main/java/com/github/iunius118/orefarmingdevice/world/level/block/OFDeviceBlock.java
+++ src/main/java/com/github/iunius118/orefarmingdevice/world/level/block/OFDeviceBlock.java
@@ -1,35 +1,64 @@
 package com.github.iunius118.orefarmingdevice.world.level.block;
 
+import com.github.iunius118.orefarmingdevice.state.properties.ModBlockStateProperties;
+import com.github.iunius118.orefarmingdevice.state.properties.OFDeviceCasing;
 import com.github.iunius118.orefarmingdevice.world.level.block.entity.OFDeviceBlockEntity;
 import com.github.iunius118.orefarmingdevice.world.level.block.entity.OFDeviceType;
 import net.minecraft.block.AbstractFurnaceBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.InventoryHelper;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.EnumProperty;
+import net.minecraft.state.StateContainer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import net.minecraftforge.common.Tags;
 
 import javax.annotation.Nullable;
 
 public class OFDeviceBlock extends AbstractFurnaceBlock {
+    public static final EnumProperty<OFDeviceCasing> CASING = ModBlockStateProperties.CASING;
+
     public final OFDeviceType type;
 
     public OFDeviceBlock(Properties properties, OFDeviceType offDeviceType) {
         super(properties);
-        registerDefaultState(this.stateDefinition.any().setValue(LIT, Boolean.FALSE));
+        registerDefaultState(stateDefinition.any().setValue(LIT, Boolean.FALSE).setValue(CASING, OFDeviceCasing.NORMAL));
         type = offDeviceType;
     }
+
     @Nullable
     @Override
     public TileEntity newBlockEntity(IBlockReader blockReader) {
         return new OFDeviceBlockEntity(type);
     }
 
+    @Override
+    public ActionResultType use(BlockState state, World level, BlockPos pos, PlayerEntity player, Hand hand, BlockRayTraceResult rayTraceResult) {
+        if (level.isClientSide) {
+            return ActionResultType.SUCCESS;
+        } else {
+            ItemStack stack = player.getItemInHand(hand);
+
+            if (Tags.Items.RODS_WOODEN.contains(stack.getItem())) {
+                OFDeviceCasing casing = state.getValue(CASING);
+                level.setBlock(pos, state.setValue(CASING, casing.getNext()), 3);
+            } else {
+                openContainer(level, pos, player);
+            }
+
+            return ActionResultType.CONSUME;
+        }
+    }
 
     @Override
     protected void openContainer(World level, BlockPos pos, PlayerEntity player) {
@@ -66,5 +95,11 @@ public class OFDeviceBlock extends AbstractFurnaceBlock {
     @Override
     public BlockRenderType getRenderShape(BlockState state) {
         return BlockRenderType.MODEL;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder);
+        builder.add(CASING);
     }
 }
