ea75946a9d918400735b19d746cb323af35941bf
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2023-11-12 01:42:36 +0900

    Change ModLootTables to use OFDeviceLootCondition

--- src/main/java/com/github/iunius118/orefarmingdevice/loot/ModLootTables.java
+++ src/main/java/com/github/iunius118/orefarmingdevice/loot/ModLootTables.java
@@ -5,39 +5,47 @@ import com.github.iunius118.orefarmingdevice.config.OreFarmingDeviceConfig;
 import com.github.iunius118.orefarmingdevice.world.item.CobblestoneFeederItem;
 import com.github.iunius118.orefarmingdevice.world.item.ModItems;
 import com.github.iunius118.orefarmingdevice.world.level.block.entity.OFDeviceBlockEntity;
-import com.github.iunius118.orefarmingdevice.world.level.block.entity.OFDeviceType;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IItemProvider;
 import net.minecraft.util.ResourceLocation;
 
-import java.util.function.Function;
+import java.util.Arrays;
+import java.util.Optional;
 
 public enum ModLootTables {
-    DEVICE_0("device_0", Blocks.COBBLESTONE, OFDeviceType.MOD_0::contains),
-    DEVICE_0_NETHER("device_0_n", Blocks.NETHERRACK, OFDeviceType.MOD_0::contains),
-    DEVICE_0_FEED("device_0", ModItems.COBBLESTONE_FEEDER, OFDeviceType.MOD_0::contains),
-    DEVICE_0_FEED_2("device_0", ModItems.COBBLESTONE_FEEDER_2, OFDeviceType.MOD_0::contains),
+    DEVICE_0("device_0", Blocks.COBBLESTONE, OFDeviceLootCondition.IS_MOD_0),
+    DEVICE_0_NETHER("device_0_n", Blocks.NETHERRACK, OFDeviceLootCondition.IS_MOD_0),
+    DEVICE_0_FEED("device_0", ModItems.COBBLESTONE_FEEDER, OFDeviceLootCondition.IS_MOD_0),
+    DEVICE_0_FEED_2("device_0", ModItems.COBBLESTONE_FEEDER_2, OFDeviceLootCondition.IS_MOD_0),
 
-    DEVICE_1("device_1", Blocks.COBBLESTONE, OFDeviceType.MOD_1::contains),
-    DEVICE_1_NETHER("device_1_n", Blocks.NETHERRACK, OFDeviceType.MOD_1::contains),
-    DEVICE_1_FEED("device_1", ModItems.COBBLESTONE_FEEDER, OFDeviceType.MOD_1::contains),
-    DEVICE_1_FEED_2("device_1", ModItems.COBBLESTONE_FEEDER_2, OFDeviceType.MOD_1::contains),
+    DEVICE_1("device_1", Blocks.COBBLESTONE, OFDeviceLootCondition.IS_MOD_1),
+    DEVICE_1_NETHER("device_1_n", Blocks.NETHERRACK, OFDeviceLootCondition.IS_MOD_1),
+    DEVICE_1_FEED("device_1", ModItems.COBBLESTONE_FEEDER, OFDeviceLootCondition.IS_MOD_1),
+    DEVICE_1_FEED_2("device_1", ModItems.COBBLESTONE_FEEDER_2, OFDeviceLootCondition.IS_MOD_1),
 
-    DEVICE_2("device_2", Blocks.COBBLESTONE, OFDeviceType.MOD_2::contains),
-    DEVICE_2_NETHER("device_2_n", Blocks.NETHERRACK, OFDeviceType.MOD_2::contains),
-    DEVICE_2_FEED("device_2", ModItems.COBBLESTONE_FEEDER, OFDeviceType.MOD_2::contains),
-    DEVICE_2_FEED_2("device_2", ModItems.COBBLESTONE_FEEDER_2, OFDeviceType.MOD_2::contains),
+    DEVICE_2("device_2", Blocks.COBBLESTONE, OFDeviceLootCondition.IS_MOD_2),
+    DEVICE_2_NETHER("device_2_n", Blocks.NETHERRACK, OFDeviceLootCondition.IS_MOD_2),
+    DEVICE_2_FEED("device_2", ModItems.COBBLESTONE_FEEDER, OFDeviceLootCondition.IS_MOD_2),
+    DEVICE_2_FEED_2("device_2", ModItems.COBBLESTONE_FEEDER_2, OFDeviceLootCondition.IS_MOD_2),
     ;
 
     private final ResourceLocation id;
     private final ItemStack material;
-    private final Function<OFDeviceBlockEntity, Boolean> canProcess;
+    private final OFDeviceLootCondition canProcess;
 
-    ModLootTables(String key, IItemProvider item, Function<OFDeviceBlockEntity, Boolean> canProcess) {
+    ModLootTables(String key, IItemProvider item, OFDeviceLootCondition canProcess) {
         id = new ResourceLocation(OreFarmingDevice.MOD_ID, key);
         this.material = new ItemStack(item);
         this.canProcess = canProcess;
+    }
+
+    public static Optional<ModLootTables> find(OFDeviceBlockEntity device, ItemStack stack) {
+        return Arrays.stream(values()).filter(t -> t.canProcess(device, stack)).findFirst();
+    }
+
+    public static Optional<ModLootTables> find(OFDeviceLootCondition lootCondition, ItemStack stack) {
+        return Arrays.stream(values()).filter(t -> t.canProcess(lootCondition, stack)).findFirst();
     }
 
     public ResourceLocation getId() {
@@ -51,6 +59,16 @@ public enum ModLootTables {
             return false;
         }
 
-        return canProcess.apply(device) && material.sameItem(stack);
+        return canProcess.test(device) && material.sameItem(stack);
+    }
+
+    public boolean canProcess(OFDeviceLootCondition lootCondition, ItemStack stack) {
+        if (material.getItem() instanceof CobblestoneFeederItem
+                && !OreFarmingDeviceConfig.SERVER.isCobblestoneFeederAvailable()) {
+            // OF Cobblestone Feeders are not available for OF Devices by config
+            return false;
+        }
+
+        return lootCondition == canProcess && material.sameItem(stack);
     }
 }
