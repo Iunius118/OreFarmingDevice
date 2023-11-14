e9a3a57db967135352f03dd2e0d744ed8d25040c
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2023-11-14 18:28:19 +0900

    Add OFDeviceLootCondition - Add data access to device - Fix item stack to move quickly to material slot - Change ModLootTables to use OFDeviceLootCondition

--- src/main/java/com/github/iunius118/orefarmingdevice/loot/ModLootTables.java
+++ src/main/java/com/github/iunius118/orefarmingdevice/loot/ModLootTables.java
@@ -11,42 +11,52 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Blocks;
 
-import java.util.function.Function;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 public enum ModLootTables {
-    DEVICE_0("device_0", Blocks.COBBLESTONE, OFDeviceType.MOD_0::contains),
-    DEVICE_0_DEEP("device_0_d", Blocks.COBBLED_DEEPSLATE, OFDeviceType.MOD_0::contains),
-    DEVICE_0_NETHER("device_0_n", Blocks.NETHERRACK, OFDeviceType.MOD_0::contains),
-    DEVICE_0_FEED("device_0", ModItems.COBBLESTONE_FEEDER, OFDeviceType.MOD_0::isDeviceInShallowLayer),
-    DEVICE_0_FEED_DEEP("device_0_d", ModItems.COBBLESTONE_FEEDER, OFDeviceType.MOD_0::isDeviceInDeepLayer),
-    DEVICE_0_FEED_2("device_0", ModItems.COBBLESTONE_FEEDER_2, OFDeviceType.MOD_0::isDeviceInShallowLayer),
-    DEVICE_0_FEED_2_DEEP("device_0_d", ModItems.COBBLESTONE_FEEDER_2, OFDeviceType.MOD_0::isDeviceInDeepLayer),
+    DEVICE_0("device_0", Blocks.COBBLESTONE, OFDeviceLootCondition.is(OFDeviceType.MOD_0)),
+    DEVICE_0_DEEP("device_0_d", Blocks.COBBLED_DEEPSLATE, OFDeviceLootCondition.is(OFDeviceType.MOD_0)),
+    DEVICE_0_NETHER("device_0_n", Blocks.NETHERRACK, OFDeviceLootCondition.is(OFDeviceType.MOD_0)),
+    DEVICE_0_FEED("device_0", ModItems.COBBLESTONE_FEEDER, OFDeviceLootCondition.is(OFDeviceLootCondition.MOD_0_IN_SHALLOW_LAYER)),
+    DEVICE_0_FEED_DEEP("device_0_d", ModItems.COBBLESTONE_FEEDER, OFDeviceLootCondition.is(OFDeviceLootCondition.MOD_0_IN_DEEP_LAYER)),
+    DEVICE_0_FEED_2("device_0", ModItems.COBBLESTONE_FEEDER_2, OFDeviceLootCondition.is(OFDeviceLootCondition.MOD_0_IN_SHALLOW_LAYER)),
+    DEVICE_0_FEED_2_DEEP("device_0_d", ModItems.COBBLESTONE_FEEDER_2, OFDeviceLootCondition.is(OFDeviceLootCondition.MOD_0_IN_DEEP_LAYER)),
 
-    DEVICE_1("device_1", Blocks.COBBLESTONE, OFDeviceType.MOD_1::contains),
-    DEVICE_1_DEEP("device_1_d", Blocks.COBBLED_DEEPSLATE, OFDeviceType.MOD_1::contains),
-    DEVICE_1_NETHER("device_1_n", Blocks.NETHERRACK, OFDeviceType.MOD_1::contains),
-    DEVICE_1_FEED("device_1", ModItems.COBBLESTONE_FEEDER, OFDeviceType.MOD_1::isDeviceInShallowLayer),
-    DEVICE_1_FEED_DEEP("device_1_d", ModItems.COBBLESTONE_FEEDER, OFDeviceType.MOD_1::isDeviceInDeepLayer),
-    DEVICE_1_FEED_2("device_1", ModItems.COBBLESTONE_FEEDER_2, OFDeviceType.MOD_1::isDeviceInShallowLayer),
-    DEVICE_1_FEED_2_DEEP("device_1_d", ModItems.COBBLESTONE_FEEDER_2, OFDeviceType.MOD_1::isDeviceInDeepLayer),
+    DEVICE_1("device_1", Blocks.COBBLESTONE, OFDeviceLootCondition.is(OFDeviceType.MOD_1)),
+    DEVICE_1_DEEP("device_1_d", Blocks.COBBLED_DEEPSLATE, OFDeviceLootCondition.is(OFDeviceType.MOD_1)),
+    DEVICE_1_NETHER("device_1_n", Blocks.NETHERRACK, OFDeviceLootCondition.is(OFDeviceType.MOD_1)),
+    DEVICE_1_FEED("device_1", ModItems.COBBLESTONE_FEEDER, OFDeviceLootCondition.is(OFDeviceLootCondition.MOD_1_IN_SHALLOW_LAYER)),
+    DEVICE_1_FEED_DEEP("device_1_d", ModItems.COBBLESTONE_FEEDER, OFDeviceLootCondition.is(OFDeviceLootCondition.MOD_1_IN_DEEP_LAYER)),
+    DEVICE_1_FEED_2("device_1", ModItems.COBBLESTONE_FEEDER_2, OFDeviceLootCondition.is(OFDeviceLootCondition.MOD_1_IN_SHALLOW_LAYER)),
+    DEVICE_1_FEED_2_DEEP("device_1_d", ModItems.COBBLESTONE_FEEDER_2, OFDeviceLootCondition.is(OFDeviceLootCondition.MOD_1_IN_DEEP_LAYER)),
 
-    DEVICE_2("device_2", Blocks.COBBLESTONE, OFDeviceType.MOD_2::contains),
-    DEVICE_2_DEEP("device_2_d", Blocks.COBBLED_DEEPSLATE, OFDeviceType.MOD_2::contains),
-    DEVICE_2_NETHER("device_2_n", Blocks.NETHERRACK, OFDeviceType.MOD_2::contains),
-    DEVICE_2_FEED("device_2", ModItems.COBBLESTONE_FEEDER, OFDeviceType.MOD_2::isDeviceInShallowLayer),
-    DEVICE_2_FEED_DEEP("device_2_d", ModItems.COBBLESTONE_FEEDER, OFDeviceType.MOD_2::isDeviceInDeepLayer),
-    DEVICE_2_FEED_2("device_2", ModItems.COBBLESTONE_FEEDER_2, OFDeviceType.MOD_2::isDeviceInShallowLayer),
-    DEVICE_2_FEED_2_DEEP("device_2_d", ModItems.COBBLESTONE_FEEDER_2, OFDeviceType.MOD_2::isDeviceInDeepLayer),
+    DEVICE_2("device_2", Blocks.COBBLESTONE, OFDeviceLootCondition.is(OFDeviceType.MOD_2)),
+    DEVICE_2_DEEP("device_2_d", Blocks.COBBLED_DEEPSLATE, OFDeviceLootCondition.is(OFDeviceType.MOD_2)),
+    DEVICE_2_NETHER("device_2_n", Blocks.NETHERRACK, OFDeviceLootCondition.is(OFDeviceType.MOD_2)),
+    DEVICE_2_FEED("device_2", ModItems.COBBLESTONE_FEEDER, OFDeviceLootCondition.is(OFDeviceLootCondition.MOD_2_IN_SHALLOW_LAYER)),
+    DEVICE_2_FEED_DEEP("device_2_d", ModItems.COBBLESTONE_FEEDER, OFDeviceLootCondition.is(OFDeviceLootCondition.MOD_2_IN_DEEP_LAYER)),
+    DEVICE_2_FEED_2("device_2", ModItems.COBBLESTONE_FEEDER_2, OFDeviceLootCondition.is(OFDeviceLootCondition.MOD_2_IN_SHALLOW_LAYER)),
+    DEVICE_2_FEED_2_DEEP("device_2_d", ModItems.COBBLESTONE_FEEDER_2, OFDeviceLootCondition.is(OFDeviceLootCondition.MOD_2_IN_DEEP_LAYER)),
     ;
 
     private final ResourceLocation id;
     private final ItemStack material;
-    private final Function<OFDeviceBlockEntity, Boolean> canProcess;
+    private final Predicate<OFDeviceLootCondition> canProcess;
 
-    ModLootTables(String key, ItemLike item, Function<OFDeviceBlockEntity, Boolean> canProcess) {
+    ModLootTables(String key, ItemLike item, Predicate<OFDeviceLootCondition> canProcess) {
         id = new ResourceLocation(OreFarmingDevice.MOD_ID, key);
         this.material = new ItemStack(item);
         this.canProcess = canProcess;
+    }
+
+    public static Optional<ModLootTables> find(OFDeviceBlockEntity device, ItemStack stack) {
+        return Arrays.stream(values()).filter(t -> t.canProcess(device, stack)).findFirst();
+    }
+
+    public static Optional<ModLootTables> find(OFDeviceLootCondition lootCondition, ItemStack stack) {
+        return Arrays.stream(values()).filter(t -> t.canProcess(lootCondition, stack)).findFirst();
     }
 
     public ResourceLocation getId() {
@@ -60,6 +70,17 @@ public enum ModLootTables {
             return false;
         }
 
-        return canProcess.apply(device) && material.is(stack.getItem());
+        OFDeviceLootCondition lootCondition = OFDeviceLootCondition.find(device);
+        return canProcess.test(lootCondition) && material.sameItem(stack);
+    }
+
+    public boolean canProcess(OFDeviceLootCondition lootCondition, ItemStack stack) {
+        if (material.getItem() instanceof CobblestoneFeederItem
+                && !OreFarmingDeviceConfig.SERVER.isCobblestoneFeederAvailable()) {
+            // OF Cobblestone Feeders are not available for OF Devices by config
+            return false;
+        }
+
+        return canProcess.test(lootCondition) && material.sameItem(stack);
     }
 }
--- src/main/java/com/github/iunius118/orefarmingdevice/world/level/block/entity/OFDeviceType.java
+++ src/main/java/com/github/iunius118/orefarmingdevice/world/level/block/entity/OFDeviceType.java
@@ -38,16 +38,4 @@ public enum OFDeviceType {
     public ResourceLocation getId() {
         return new ResourceLocation(OreFarmingDevice.MOD_ID, name);
     }
-
-    public boolean contains(OFDeviceBlockEntity device) {
-        return device != null && device.type == this;
-    }
-
-    public boolean isDeviceInShallowLayer(OFDeviceBlockEntity device) {
-        return contains(device) && device.getBlockPos().getY() > 0;
-    }
-
-    public boolean isDeviceInDeepLayer(OFDeviceBlockEntity device) {
-        return contains(device) && device.getBlockPos().getY() <= 0;
-    }
 }
