6bbfd3c453da4ed682ea4e2cd5f43832a6f9556e
Iunius118 <18502177+Iunius118@users.noreply.github.com>
2023-11-14 22:02:14 +0900

    Add block property `casing` to OF Devices to change appearance of device

--- src/main/java/com/github/iunius118/orefarmingdevice/world/level/block/ModBlocks.java
+++ src/main/java/com/github/iunius118/orefarmingdevice/world/level/block/ModBlocks.java
@@ -1,7 +1,6 @@
 package com.github.iunius118.orefarmingdevice.world.level.block;
 
 import com.github.iunius118.orefarmingdevice.world.level.block.entity.OFDeviceType;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.MapColor;
@@ -10,7 +9,7 @@ public class ModBlocks {
     private static final BlockBehaviour.Properties ofDevicePropertiesSupplier = BlockBehaviour.Properties.of()
             .mapColor(MapColor.STONE).instrument(NoteBlockInstrument.BASEDRUM).requiresCorrectToolForDrops().strength(3.5F);
 
-    public static final Block DEVICE_0 = new OFDeviceBlock(ofDevicePropertiesSupplier, OFDeviceType.MOD_0);
-    public static final Block DEVICE_1 = new OFDeviceBlock(ofDevicePropertiesSupplier, OFDeviceType.MOD_1);
-    public static final Block DEVICE_2 = new OFDeviceBlock(ofDevicePropertiesSupplier, OFDeviceType.MOD_2);
+    public static final OFDeviceBlock DEVICE_0 = new OFDeviceBlock(ofDevicePropertiesSupplier, OFDeviceType.MOD_0);
+    public static final OFDeviceBlock DEVICE_1 = new OFDeviceBlock(ofDevicePropertiesSupplier, OFDeviceType.MOD_1);
+    public static final OFDeviceBlock DEVICE_2 = new OFDeviceBlock(ofDevicePropertiesSupplier, OFDeviceType.MOD_2);
 }
