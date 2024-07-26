0db045bf13af7279012282d9f56b4e3a4abd8472
iunius118 <18502177+Iunius118@users.noreply.github.com>
2024-07-27 00:10:56 +0900

    Add OF C Device

--- src/main/java/com/github/iunius118/orefarmingdevice/common/RegisterEventHandler.java
+++ src/main/java/com/github/iunius118/orefarmingdevice/common/RegisterEventHandler.java
@@ -2,11 +2,13 @@ package com.github.iunius118.orefarmingdevice.common;
 
 import com.github.iunius118.orefarmingdevice.OreFarmingDevice;
 import com.github.iunius118.orefarmingdevice.inventory.ModMenuTypes;
+import com.github.iunius118.orefarmingdevice.world.item.CobblestoneFeederType;
 import com.github.iunius118.orefarmingdevice.world.item.ModCreativeModeTabs;
 import com.github.iunius118.orefarmingdevice.world.item.ModItems;
 import com.github.iunius118.orefarmingdevice.world.item.crafting.ModRecipeSerializers;
 import com.github.iunius118.orefarmingdevice.world.item.crafting.ModRecipeTypes;
 import com.github.iunius118.orefarmingdevice.world.level.block.ModBlocks;
+import com.github.iunius118.orefarmingdevice.world.level.block.entity.CobblestoneDeviceType;
 import com.github.iunius118.orefarmingdevice.world.level.block.entity.ModBlockEntityTypes;
 import com.github.iunius118.orefarmingdevice.world.level.block.entity.OFDeviceType;
 import net.minecraft.core.registries.Registries;
@@ -26,35 +28,38 @@ public class RegisterEventHandler {
     }
 
     private static void registerBlocks(IEventBus modEventBus) {
-        var blockDeferredRegister = DeferredRegister.create(ForgeRegistries.BLOCKS, OreFarmingDevice.MOD_ID);
+        var blockRegister = DeferredRegister.create(ForgeRegistries.BLOCKS, OreFarmingDevice.MOD_ID);
 
-        blockDeferredRegister.register(OFDeviceType.MOD_0.getName(), () -> ModBlocks.DEVICE_0);
-        blockDeferredRegister.register(OFDeviceType.MOD_1.getName(), () -> ModBlocks.DEVICE_1);
-        blockDeferredRegister.register(OFDeviceType.MOD_2.getName(), () -> ModBlocks.DEVICE_2);
+        blockRegister.register(OFDeviceType.MOD_0.getName(), () -> ModBlocks.DEVICE_0);
+        blockRegister.register(OFDeviceType.MOD_1.getName(), () -> ModBlocks.DEVICE_1);
+        blockRegister.register(OFDeviceType.MOD_2.getName(), () -> ModBlocks.DEVICE_2);
+        blockRegister.register(CobblestoneDeviceType.BASIC.getName(), () -> ModBlocks.COBBLESTONE_DEVICE_0);
 
-        blockDeferredRegister.register(modEventBus);
+        blockRegister.register(modEventBus);
     }
 
     private static void registerItems(IEventBus modEventBus) {
-        var itemDeferredRegister = DeferredRegister.create(ForgeRegistries.ITEMS, OreFarmingDevice.MOD_ID);
+        var itemRegister = DeferredRegister.create(ForgeRegistries.ITEMS, OreFarmingDevice.MOD_ID);
 
-        itemDeferredRegister.register(OFDeviceType.MOD_0.getName(), () -> ModItems.DEVICE_0);
-        itemDeferredRegister.register(OFDeviceType.MOD_1.getName(), () -> ModItems.DEVICE_1);
-        itemDeferredRegister.register(OFDeviceType.MOD_2.getName(), () -> ModItems.DEVICE_2);
-        itemDeferredRegister.register("cobblestone_feeder", () -> ModItems.COBBLESTONE_FEEDER);
-        itemDeferredRegister.register("cobblestone_feeder_2", () -> ModItems.COBBLESTONE_FEEDER_2);
+        itemRegister.register(OFDeviceType.MOD_0.getName(), () -> ModItems.DEVICE_0);
+        itemRegister.register(OFDeviceType.MOD_1.getName(), () -> ModItems.DEVICE_1);
+        itemRegister.register(OFDeviceType.MOD_2.getName(), () -> ModItems.DEVICE_2);
+        itemRegister.register(CobblestoneDeviceType.BASIC.getName(), () -> ModItems.COBBLESTONE_DEVICE_0);
+        itemRegister.register(CobblestoneFeederType.BASIC.getName(), () -> ModItems.COBBLESTONE_FEEDER);
+        itemRegister.register(CobblestoneFeederType.UPGRADED.getName(), () -> ModItems.COBBLESTONE_FEEDER_2);
 
-        itemDeferredRegister.register(modEventBus);
+        itemRegister.register(modEventBus);
     }
 
     private static void registerBlockEntityTypes(IEventBus modEventBus) {
-        var blockEntityTypeDeferredRegister = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, OreFarmingDevice.MOD_ID);
+        var blockEntityTypeRegister = DeferredRegister.create(ForgeRegistries.BLOCK_ENTITY_TYPES, OreFarmingDevice.MOD_ID);
 
-        blockEntityTypeDeferredRegister.register(OFDeviceType.MOD_0.getName(), () -> ModBlockEntityTypes.DEVICE_0);
-        blockEntityTypeDeferredRegister.register(OFDeviceType.MOD_1.getName(), () -> ModBlockEntityTypes.DEVICE_1);
-        blockEntityTypeDeferredRegister.register(OFDeviceType.MOD_2.getName(), () -> ModBlockEntityTypes.DEVICE_2);
+        blockEntityTypeRegister.register(OFDeviceType.MOD_0.getName(), () -> ModBlockEntityTypes.DEVICE_0);
+        blockEntityTypeRegister.register(OFDeviceType.MOD_1.getName(), () -> ModBlockEntityTypes.DEVICE_1);
+        blockEntityTypeRegister.register(OFDeviceType.MOD_2.getName(), () -> ModBlockEntityTypes.DEVICE_2);
+        blockEntityTypeRegister.register(CobblestoneDeviceType.BASIC.getName(), () -> ModBlockEntityTypes.COBBLESTONE_DEVICE_0);
 
-        blockEntityTypeDeferredRegister.register(modEventBus);
+        blockEntityTypeRegister.register(modEventBus);
     }
 
     private static void registerRecipeTypes(IEventBus modEventBus) {
@@ -66,26 +71,27 @@ public class RegisterEventHandler {
     }
 
     private static void registerRecipeSerializers(IEventBus modEventBus) {
-        var recipeSerializerDeferredRegister = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, OreFarmingDevice.MOD_ID);
+        var recipeSerializerRegister = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, OreFarmingDevice.MOD_ID);
 
-        recipeSerializerDeferredRegister.register("device_processing", () -> ModRecipeSerializers.DEVICE_PROCESSING);
+        recipeSerializerRegister.register("device_processing", () -> ModRecipeSerializers.DEVICE_PROCESSING);
 
-        recipeSerializerDeferredRegister.register(modEventBus);
+        recipeSerializerRegister.register(modEventBus);
     }
 
     private static void registerMenuTypes(IEventBus modEventBus) {
-        var menuTypeDeferredRegister = DeferredRegister.create(ForgeRegistries.MENU_TYPES, OreFarmingDevice.MOD_ID);
+        var menuTypeRegister = DeferredRegister.create(ForgeRegistries.MENU_TYPES, OreFarmingDevice.MOD_ID);
 
-        menuTypeDeferredRegister.register("device", () -> ModMenuTypes.DEVICE);
+        menuTypeRegister.register("device", () -> ModMenuTypes.DEVICE);
+        menuTypeRegister.register("cobblestone_device", () -> ModMenuTypes.COBBLESTONE_DEVICE);
 
-        menuTypeDeferredRegister.register(modEventBus);
+        menuTypeRegister.register(modEventBus);
     }
 
     private static void registerCreativeModeTabs(IEventBus modEventBus) {
-        var creativeModeTabDeferredRegister = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, OreFarmingDevice.MOD_ID);
+        var creativeModeTabRegister = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, OreFarmingDevice.MOD_ID);
 
-        creativeModeTabDeferredRegister.register("general", () -> ModCreativeModeTabs.MAIN);
+        creativeModeTabRegister.register("general", () -> ModCreativeModeTabs.MAIN);
 
-        creativeModeTabDeferredRegister.register(modEventBus);
+        creativeModeTabRegister.register(modEventBus);
     }
 }
--- src/main/java/com/github/iunius118/orefarmingdevice/inventory/ModMenuTypes.java
+++ src/main/java/com/github/iunius118/orefarmingdevice/inventory/ModMenuTypes.java
@@ -5,4 +5,5 @@ import net.minecraft.world.inventory.MenuType;
 
 public class ModMenuTypes {
     public static final MenuType<OFDeviceMenu> DEVICE = new MenuType<>(OFDeviceMenu::new, FeatureFlags.DEFAULT_FLAGS);
+    public static final MenuType<CobblestoneDeviceMenu> COBBLESTONE_DEVICE = new MenuType<>(CobblestoneDeviceMenu::new, FeatureFlags.DEFAULT_FLAGS);
 }
