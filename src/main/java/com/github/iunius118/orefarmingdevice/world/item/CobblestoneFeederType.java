9e6926d698db65f853c42a0a0f9178aa8c0367a8
iunius118 <18502177+Iunius118@users.noreply.github.com>
2024-07-28 02:32:37 +0900

    Refactor

--- src/main/java/com/github/iunius118/orefarmingdevice/world/item/CobblestoneFeederType.java
+++ src/main/java/com/github/iunius118/orefarmingdevice/world/item/CobblestoneFeederType.java
@@ -1,7 +1,24 @@
 package com.github.iunius118.orefarmingdevice.world.item;
 
+import com.github.iunius118.orefarmingdevice.OreFarmingDevice;
+import net.minecraft.resources.ResourceLocation;
+
 public enum CobblestoneFeederType {
-    BASIC,
-    UPGRADED
+    BASIC("cobblestone_feeder"),
+    UPGRADED("cobblestone_feeder_2"),
     ;
+
+    private final String name;
+
+    CobblestoneFeederType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ResourceLocation getId() {
+        return OreFarmingDevice.makeId(name);
+    }
 }
